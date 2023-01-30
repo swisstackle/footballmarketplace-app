6dab693b9dbb2657a3acb4037a424b9f393f8cc4
swisstackle <alainsch@buffalo.edu>
2023-01-30 14:38:58 -0500

    Devops changes

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -1,36 +1,38 @@
-import express from "express";
-import path from "path";
-import winston from "winston";
+import express from 'express';
+import path from 'path';
+import winston from 'winston';
 // import serviceRoutes from "routes/service";
 // import userRoutes from "routes/user";
 
-const port : number = 3000;
+const port: number = 3000;
 const app = express();
 app.use(express.json());
 
 const logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.json(),
-    defaultMeta: { service: 'user-service' },
-    transports: [
-      new winston.transports.File({ filename: 'error.log', level: 'error' }),
-      new winston.transports.File({ filename: 'combined.log' }),
-      new winston.transports.Console(),
-    ],
-  });
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+    new winston.transports.Console()
+  ]
+});
 
-  if (process.env.NODE_ENV !== 'production') {
-    logger.add(new winston.transports.Console({
-      format: winston.format.simple(),
-    }));
-  }
-app.get('/',(req, res, next) => {
-    res.send("Root route is working");
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple()
+    })
+  );
+}
+app.get('/', (req, res, next) => {
+  res.send('Root route is working');
 });
 
 module.exports = app.listen(port, () => {
-    logger.log({
-        level: 'info',
-        message: 'Server started at http://localhost:${port}'
-    });
+  logger.log({
+    level: 'info',
+    message: 'Server started at http://localhost:${port}'
+  });
 });
