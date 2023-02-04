2ea2b048270cde87467e00536cbcb6c6ed27a43f
Alain Schaerer <alainsch@buffalo.edu>
2023-02-04 17:47:38 -0500

    Change: Jest as test framework now. Deleted frontend.

--- backend/test/test.spec.ts
+++ backend/test/test.spec.ts
@@ -1,28 +1,9 @@
 import server from '../src/api/index';
-import chai from "chai";
-import chaiHttp from "chai-http";
-import {describe } from 'mocha';
-chai.should();
-chai.use(chaiHttp);
+import {describe, it, jest } from '@jest/globals';
+
 
 describe("Happy Tests", () => {
-    it("Server works", (done) => {
-        chai.request(server)
-        .get("/")
-        .end((err: Error, response) => {
-            response.status.should.be.equal(200);
-            response.text.should.be.equal("Root route is working");
-            done();
-        });
-    });
-    it("Register User", (done) => {
-        chai.request(server)
-        .post("/users/register", )
-        .send({username:"alain", password:"mypassword", address:"myaddress"})
-        .end((err: Error, response) => {
-            response.status.should.be.equal(200);
-            response.text.should.be.equal("Success");
-            done();
-        });
+    it("testest",() => {
+
     });
 });
